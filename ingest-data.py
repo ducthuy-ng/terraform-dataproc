44b405cd0751db42378e433e1e432323bea3d3a6
Thuy Nguyen <ducthuynguyen360@yahoo.com>
2024-11-18 23:32:53 +0700

    Merge all code into single file

--- ingest-data.py
+++ ingest-data.py
@@ -175,6 +175,49 @@ def append_sale_headers(spark: SparkSession, input_file, output_table):
     df.write.format("bigquery").mode("append").option("table", output_table).save()
 
 
+def append_sale_lines(spark: SparkSession, input_file, output_table):
+    schema = [
+        "INVOICE_DATE DATE",
+        "STORE_CODE INTEGER",
+        "TILL_NO INTEGER",
+        "SALE_INVC_TYPE INTEGER",
+        "INVOICE_NO INTEGER",
+        "SALE_LINE_NO INTEGER",
+        "SALE_LINE_TYPE STRING",
+        "PRODUCT_CODE STRING",
+        "SALE_TOT_QTY FLOAT",
+        "SALE_NET_VAL FLOAT",
+        "SALE_TOT_TAX_VAL FLOAT",
+        "ACTUAL_PRODUCT_PRICE FLOAT",
+        "PRODUCT_PRICE FLOAT",
+        "SALE_VAL_AT_COST FLOAT",
+        "SALE_TOT_DISC_VAL FLOAT",
+        "RETURN_REASON_CODE STRING",
+        "PROMOTION_CODE STRING",
+        "PROMO_OFFER_PRICE FLOAT",
+        "DELIVERY_STORE_CODE INTEGER",
+        "SHIPMENT_METHOD_CODE STRING",
+        "SHIPMENT_CHANNEL_CODE STRING",
+        "SESSION_ID INTEGER",
+        "ORDER_DATE DATE",
+        "ORDER_ID INTEGER",
+        "ORDER_LINE_NO INTEGER",
+        "OFFER_CODE STRING",
+        "REEDEMED_COUPON_CODE STRING",
+    ]
+    schema_str = ", ".join(schema)
+
+    df = (
+        spark.read.format("csv")
+        .option("delimiter", "|")
+        .option("encoding", "ISO-8859-1")
+        .option("header", True)
+        .schema(schema_str)
+        .load(input_file)
+    )
+    df.write.format("bigquery").mode("append").option("table", output_table).save()
+
+
 if __name__ == "__main__":
     conf = (
         SparkConf()
@@ -197,6 +240,12 @@ if __name__ == "__main__":
     )
 
     append_products(
+        spark,
+        "gs://retailer-raw-ingest-data/SalesLineFCT_inc_20240919.txt",
+        "modified-glyph-438213-k8.spark_retailer_dataset.sales_lines",
+    )
+
+    append_sale_lines(
         spark,
         "gs://retailer-raw-ingest-data/SalesLineFCT_inc_20240919.txt",
         "modified-glyph-438213-k8.spark_retailer_dataset.sales_lines",
